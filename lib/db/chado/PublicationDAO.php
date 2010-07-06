dbcb0290f8f6b1df7b42d678f1ca307c20b317c5
Randall Svancara <rsvancara@gmail.com>
2010-07-06 14:43:31 -0700

    Adding new publication data object.  More data access objects to come....

---
+++ lib/db/chado/PublicationDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Publication Data Access Object. 
+ * Data access object that provides methods for interfacing with
+ * the chado database publication tables.
+ *
+ * All these classes rely on PDO, make sure it is installed.
+ * 
+ * @author Randall Svancara
+ */
+
+
+
+
+
