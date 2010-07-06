5592114df3e85c493206b0290315e61560561c6b
Randall Svancara <rsvancara@gmail.com>
2010-07-06 15:01:54 -0700

    More changes to the structure of this library.
    Randall

---
+++ lib/biophp/db/chado/BaseDAO.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace biophp/db/chado/BaseDAO 
+
+/** 
+ * Base data access object
+ * All Chado data access objects must inherit from this class
+ * 
+ * @author Randall Svancara
+ */
+
+class BaseDAO {
+
+
+
+
+}
