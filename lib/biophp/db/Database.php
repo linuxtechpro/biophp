c0a823298649418c9174ea371010818f34849a6a
Randall Svancara <rsvancara@gmail.com>
2010-07-12 07:09:39 -0700

    Adding more changes

--- lib/biophp/db/Database.php
+++ lib/biophp/db/Database.php
@@ -1,12 +1,55 @@
 <?php
 
+namespace biophp\db;
+
+/**
+ * Primary front-controller for the database system.
+ *
+ * This class is uninstantiatable and un-extendable. It acts to encapsulate
+ * all control and shepherding of database connections into a single location
+ * without the use of globals.
+ *
+ * Ideas borrowed from Drupal 7 database API
+ * 
+ */
+
+
 abstract class Database {
+
+
+  /**
+   * An nested array of all active connections. It is keyed by database name
+   * and target.
+   *
+   * @var array
+   */
     
+  static protected $connections = array();
     
-    public final static function getConnection() {
+  /**
+   * A processed copy of the database connection information from settings.php.
+   *
+   * @var array
+   */
+  static protected $databaseInfo = NULL;
+  
+  
+  
+  /**
+   * Gets the connection object for the specified database key and target.
+   *
+   * @param $target
+   *   The database target name.
+   * @param $key
+   *   The database connection key. Defaults to NULL which means the active key.
+   *
+   * @return DatabaseConnection
+   *   The corresponding connection object.
+   */
+  public final static function getConnection() {
         
         
         
-    }
+  }
     
 }
