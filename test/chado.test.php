7a4c7533036ad785a31ba4bef44c8b892b2ff726
Randall Svancara <randalls@bioinfo.wsu.edu>
2010-07-14 11:38:15 -0700

    Adding chado.test.php back

---
+++ test/chado.test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Chado database tests
+ */
+
+// Load Bootstrap
+require_once "../lib/Bootstrap.php";
+
+Bootstrap::Initialize(0,"/etc/biophp/config.ini");
+
+$config = biophp\Config::getInstance();
+
+// Get a list of database profiles
+$config_array = $config->getDatabaseProfilesList();
+echo var_dump($config_array);
+
+// Get information about a profile
+$config_array = $config->getDatabaseProfile("database_profile");
+echo var_dump($config_array);
+
+// Set the default profile to use.
+$config->dbprofile = "database_profile";
+
+print "\nThe default profile is: " .  $config->dbprofile . "\n";
+
+$result = biophp\db\Database::getConnection()
+  ->query("select * from pub",array(),array());
+
+while($row = $result->fetchObject()) {
+  print $row->pub_id . "\n";
+}
+
+$select = new biophp\db\SelectQuery("pub","p");
+$select->addField("pub_id","pub_id","p");
+$select->addField("title","title","p");
